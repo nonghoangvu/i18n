377fdcac2a7146e96c4b7efe601f1897ccfe33b3
Nong Hoang Vu <nonghoangvu2004@gmail.com>
2025-11-24 17:04:27 +0700

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# 1. Setting
+```text
+C:\<user>\.m2\settings.xml
+```
+`settings.xml`
+```xml
+<settings>
+  <servers>
+    <server>
+      <id>github</id>
+      <username>nonghoangvu</username>
+      <password>https://github.com/settings/tokens</password>
+    </server>
+  </servers>
+</settings>
+```
+
+# 2. Package
+``mvn clean deploy``
--- src/main/java/vn/i18n/I18NConfig.java
+++ src/main/java/vn/i18n/I18NConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 public class I18NConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(I18NConfig.class);
+    private static final Logger log = LoggerFactory.getLogger("I18N-INFO");
 
     @Bean
     public I18N messageUtils(org.springframework.context.MessageSource messageSource) {
-        log.info("Source: {}", "https://github.com/nonghoangvu/i18n");
+        log.info("{}", "https://github.com/nonghoangvu/i18n");
         return new I18N(messageSource);
     }
 }
